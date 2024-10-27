cbe916ac7527b4c15f1c9b4ae574baf75e8f9d6c
Ari-Ameachi <Ari.Ameachi@city.ac.uk>
2024-10-27 16:19:15 +0000

    Add files via upload

--- HW 4/task 2.sql
+++ HW 4/task 2.sql
@@ -1,1 +1,20 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics'),
+       ('Hammy', '2020-10-15', 'vet', 'antibiotics');
+UPDATE petPet
+SET birth = '2010-10-30'
+WHERE petname = 'Hammy';
 
+INSERT INTO petPet (petname, owner, species, gender, birth, death)
+VALUES ('FluffyKitten1', 'OwnerOfFluffy', 'cat', 'M', '2020-10-15', NULL),
+       ('FluffyKitten2', 'OwnerOfFluffy', 'cat', 'M', '2020-10-15', NULL),
+       ('FluffyKitten3', 'OwnerOfFluffy', 'cat', 'F', '2020-10-15', NULL);
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Claws', '1997-08-03', 'injury', 'broke rib');
+
+UPDATE petPet
+SET death = '2020-09-01'
+WHERE petname = 'Puffball';
+
+DELETE FROM petPet
+WHERE petname = 'HaroldsDog';
