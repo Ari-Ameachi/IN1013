0d0eec3302eeeebb6c620bf73676723810610cf4
Ari-Ameachi <Ari.Ameachi@city.ac.uk>
2024-11-24 14:34:28 +0000

    Add files via upload

--- HW 6/task4.sql
+++ HW 6/task4.sql
@@ -1,1 +1,41 @@
+-- 1
 
+SELECT DISTINCT b.cust_name
+FROM restBill b
+JOIN restRoom_management rm ON b.bill_date = rm.room_date
+JOIN restStaff s ON rm.headwaiter = s.staff_no
+WHERE s.first_name = 'Charles' AND b.bill_total > 450.00;
+
+-- 2
+
+SELECT DISTINCT s.first_name, s.surname
+FROM restRoom_management rm
+JOIN restStaff s ON rm.headwaiter = s.staff_no
+WHERE rm.room_date = 160111 AND EXISTS (
+    SELECT 1
+    FROM restBill b
+    WHERE b.bill_date = 160111 AND b.cust_name = 'Nerida Smith'
+);
+
+-- 3
+
+SELECT b.cust_name
+FROM restBill b
+WHERE b.bill_total = (SELECT MIN(bill_total) FROM restBill);
+
+-- 4
+
+SELECT s.first_name, s.surname
+FROM restStaff s
+WHERE s.staff_no NOT IN (SELECT DISTINCT waiter_no FROM restBill);
+
+-- 5
+
+SELECT b.cust_name, hs.first_name AS headwaiter_first_name, hs.surname AS headwaiter_surname, rm.room_name
+FROM restBill b
+JOIN restRest_table rt ON b.table_no = rt.table_no
+JOIN restRoom_management rm ON rt.room_name = rm.room_name AND b.bill_date = rm.room_date
+JOIN restStaff hs ON rm.headwaiter = hs.staff_no
+WHERE b.bill_total = (SELECT MAX(bill_total) FROM restBill);
+
+…
--- HW 6/task5.sql
+++ HW 6/task5.sql
@@ -1,1 +1,47 @@
+-- 1
 
+SELECT s.first_name, s.surname, b.bill_date, COUNT(*) AS number_of_bills
+FROM restBill b
+JOIN restStaff s ON b.waiter_no = s.staff_no
+GROUP BY s.first_name, s.surname, b.bill_date
+HAVING COUNT(*) >= 2;
+
+-- 2
+
+SELECT rt.room_name, COUNT(*) AS number_of_tables
+FROM restRest_table rt
+WHERE rt.no_of_seats > 6
+GROUP BY rt.room_name;
+
+-- 3
+
+SELECT rt.room_name, SUM(b.bill_total) AS total_bill_amount
+FROM restBill b
+JOIN restRest_table rt ON b.table_no = rt.table_no
+GROUP BY rt.room_name;
+
+-- 4
+
+SELECT hs.first_name AS headwaiter_first_name, hs.surname AS headwaiter_surname, SUM(b.bill_total) AS total_bill_amount
+FROM restBill b
+JOIN restStaff ws ON b.waiter_no = ws.staff_no
+JOIN restStaff hs ON ws.headwaiter = hs.staff_no
+GROUP BY hs.first_name, hs.surname
+ORDER BY total_bill_amount DESC;
+
+-- 5
+
+SELECT b.cust_name
+FROM restBill b
+GROUP BY b.cust_name
+HAVING AVG(b.bill_total) > 400;
+
+-- 6
+
+SELECT s.first_name, s.surname, b.bill_date, COUNT(*) AS number_of_bills
+FROM restBill b
+JOIN restStaff s ON b.waiter_no = s.staff_no
+GROUP BY s.first_name, s.surname, b.bill_date
+HAVING COUNT(*) >= 3;
+
+…
